34d6979adc43a5e7355af477d93c95d962cca508
calipsso <kamil.vizy@gmail.com>
2024-01-09 18:24:28 +0100

    upload

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("OOP")
+student1 = ("jana", 17)
+student2 = ("karol", 70)
+student3 = ("beata", 69)
+print(type(student3))
+
+studenti = [student1, student2, student3]
+print(type(studenti))
+
+def najstarsi(studenti):
+  return studenti[1]
+
+
+
+max_vek = max(studenti, key=najstarsi)
+
+print(max_vek)
+
+users = {
+  "meno":"kamil",
+  "vek":86,
+  "zamestnanie":"ucitel",
+  "aktivny":True
+}
+users["vyska"] = 180
+print(users)
