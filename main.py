6dca7e138688df3378ed3ca226b3b594a6b78431
calipsso <kamil.vizy@gmail.com>
2024-01-09 20:10:15 +0100

    OOP - CLASS person, vytvorenie objektov

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 class Person:
-    def __init__(self, name, age, gender, location, job):
-        self.name = name
-        self.age = age
+    def __init__(self, name, age, gender, location, job, farba_oci):
+        self.__name = name # __ 2x podciarkovnik je private neda sa zmenit, pristupit mozem len zavolanim funkci
+        self.age = age # publick viem pristupovat k premennej aj mimo class
         self.gender = gender
         self.location = location
-        self.job = job
+        self._job = job # 1x podciarkovnik je protected
+        self.oci_farba = farba_oci e
 
     def pozdrav(self):
-        print(f"Ahoj volam sa {self.name} a mam {self.age} rokov")
+        print(f"Ahoj volam sa {self.__name} a mam {self.age} rokov")
 
     def bydlisko(self):
         print(f"bydlisko je {self.location}")
 
     def praca(self):
-        print(f"praca je {self.job}")
+        print(f"praca je {self._job}")
 
     def zostarnutie(self, pocet_rokov):
         vek = self.age + pocet_rokov
         print(f"Patrik zostarol o {pocet_rokov} a mas {vek}")
 
 
-patrik = Person("Patrik", 30, "muz", "Bratislava", "szco")
+
+patrik = Person("Patrik", 30, "muz", "Bratislava", "szco", "modra")
+patrik.name = "Zmena"
+
 patrik.pozdrav()
 patrik.bydlisko()
 patrik.praca()
 patrik.zostarnutie(7)
+print(patrik.oci_farba)
 
 
-kamil = Person("kamil", 34, "muz", "Bratislava", "IT")
+kamil = Person("kamil", 34, "muz", "Bratislava", "IT", "zelena")
 kamil.pozdrav()
 kamil.bydlisko()
 kamil.praca()
